df473f87a0f8b23b07ceeec1569565da45dca368
gandy818 <gandy818@gmail.com>
2023-10-17 23:06:06 +0900

    feat: 문제 개수 설정 페이지 추가

---
+++ src/app/count/page.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+export default function Count() {
+    const [count, setCount] = useState(10);
+    const router = useRouter();
+
+    return (
+        <div className="max-w-[360px] mx-auto py-10 ">
+            <div className="text-center font-bold text-xl">하나둘셋 땡!</div>
+
+            <div className="text-center mt-10 text-lg">
+                문제 갯수를 설정해주세요
+            </div>
+
+            {/* 타이머 */}
+            <div className="mt-48">
+                <div className="flex">
+                    {/* 빼기 */}
+                    <div
+                        className="self-center"
+                        onClick={() => setCount((count) => count - 1)}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="44"
+                            height="44"
+                            viewBox="0 0 24 24"
+                            className="cursor-pointer"
+                        >
+                            <path
+                                fill="currentColor"
+                                d="M18 11H6a2 2 0 0 0 0 4h12a2 2 0 0 0 0-4z"
+                            />
+                        </svg>
+                    </div>
+
+                    {/* 타이머 */}
+                    <div className="text-9xl font-bold mx-auto w-fit my-5">
+                        {count}
+                    </div>
+
+                    {/* 추가 */}
+                    <div
+                        className="self-center"
+                        onClick={() => setCount((count) => count + 1)}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="44"
+                            height="44"
+                            viewBox="0 0 24 24"
+                            className="cursor-pointer"
+                        >
+                            <path
+                                fill="currentColor"
+                                d="M18 10h-4V6a2 2 0 0 0-4 0l.071 4H6a2 2 0 0 0 0 4l4.071-.071L10 18a2 2 0 0 0 4 0v-4.071L18 14a2 2 0 0 0 0-4z"
+                            />
+                        </svg>
+                    </div>
+                </div>
+
+                {/* 시작 버튼 */}
+                <div className="mx-auto mt-52">
+                    <div
+                        className="btn btn-outline btn-error w-full"
+                        onClick={() => router.push('/movies/blank')}
+                    >
+                        start
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,57 @@
-"use client";
+'use client';
 
-import { useRouter } from "next/navigation";
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  const router = useRouter();
+    const router = useRouter();
 
-  return (
-    <div className="max-w-[360px] mx-auto mt-10 ">
-      <div className="text-center font-bold text-xl">하나둘셋 땡!</div>
-      {/* 카드 리스트 */}
-      <div className="mt-10">
-        <div className="card bg-base-100 shadow-xl my-5">
-          <div className="card-body">
-            <h2 className="card-title">영화 명대사 맞추기</h2>
-            <div className="card-actions justify-end">
-              <button
-                className="btn btn-error btn-outline btn-sm text-xs"
-                onClick={() => router.push("/timer")}
-              >
-                Start
-              </button>
-            </div>
-          </div>
-        </div>
+    return (
+        <div className="max-w-[360px] mx-auto mt-10 ">
+            <div className="text-center font-bold text-xl">하나둘셋 땡!</div>
+            {/* 카드 리스트 */}
+            <div className="mt-10">
+                <div className="card bg-base-100 shadow-xl my-5">
+                    <div className="card-body">
+                        <h2 className="card-title">영화 명대사 맞추기</h2>
+                        <div className="card-actions justify-end">
+                            <button
+                                className="btn btn-error btn-outline btn-sm text-xs"
+                                onClick={() => router.push('/timer')}
+                            >
+                                Start
+                            </button>
+                        </div>
+                    </div>
+                </div>
 
-        <div className="card bg-base-100 shadow-xl my-5">
-          <div className="card-body">
-            <h2 className="card-title">영화 명대사 빈칸 맞추기</h2>
-            <div className="card-actions justify-end">
-              <button
-                className="btn btn-error btn-outline btn-sm text-xs "
-                onClick={() => router.push("/movies/blank")}
-              >
-                Start
-              </button>
-            </div>
-          </div>
-        </div>
+                <div className="card bg-base-100 shadow-xl my-5">
+                    <div className="card-body">
+                        <h2 className="card-title">영화 명대사 빈칸 맞추기</h2>
+                        <div className="card-actions justify-end">
+                            <button
+                                className="btn btn-error btn-outline btn-sm text-xs "
+                                onClick={() => router.push('/count')}
+                            >
+                                Start
+                            </button>
+                        </div>
+                    </div>
+                </div>
 
-        <div className="card bg-base-100 shadow-xl my-5">
-          <div className="card-body">
-            <h2 className="card-title">노래 제목 맞추기</h2>
-            <div className="card-actions justify-end">
-              <button
-                className="btn btn-error btn-outline btn-sm text-xs"
-                onClick={() => router.push("/timer")}
-              >
-                Start
-              </button>
+                <div className="card bg-base-100 shadow-xl my-5">
+                    <div className="card-body">
+                        <h2 className="card-title">노래 제목 맞추기</h2>
+                        <div className="card-actions justify-end">
+                            <button
+                                className="btn btn-error btn-outline btn-sm text-xs"
+                                onClick={() => router.push('/timer')}
+                            >
+                                Start
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
